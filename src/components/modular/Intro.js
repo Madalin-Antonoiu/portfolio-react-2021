a1e51ae4bc165fc2a04c38fea12459892cffaa69
@madalin-antonoiu <antonoiumadalin@gmail.com>
2021-03-05 10:24:50 +0000

    Updated title

--- src/components/modular/Intro.js
+++ src/components/modular/Intro.js
@@ -5,7 +5,7 @@ import Image from "react-graceful-image";
 const Intro = () => {
     return <section className="intro" id="home">
         <h1 className="section__title section__title--intro">
-            Hi, I am <strong>Madalin Antonoiu</strong>
+            Hi, I am <strong className="my-name">Madalin Antonoiu</strong>
         </h1>
         <p className="section__subtitle section__subtitle--intro">software developer</p>
 
